67a62a9c6cea99dafcb7d451a1b36ea7684e9fde
sajmon <szymon17099@gmail.com>
2025-06-23 18:55:14 +0200

    Prevent Discord from changing microphone volume

--- configuration.nix
+++ configuration.nix
@@ -163,12 +163,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.extraConfig."99-stop-microphone-auto-adjust.conf" = {
+      "access.rules" = [
+        {
+          matches = [
+            { "application.process.binary" = "discord"; }
+            { "application.process.binary" = "vencord"; }
+            { "application.process.binary" = "vesktop"; }
+          ];
+          actions = {
+            update-props = {
+              default_permissions = "rx";
+            };
+          };
+        }
+      ];
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
